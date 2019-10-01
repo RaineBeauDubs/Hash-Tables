4263d925818088e5e2b8324d1b9e66fc97cb8471
Raine Wallace <rainebeaudubs@gmail.com>
2019-10-01 18:59:18 -0400

    if stretch was just implementing a djb2 hash instead of the Python hash that was given, then I may have actually gotten a 3 on this project. Did that, and cleaned up code and comments.

--- src/hashtable.py
+++ src/hashtable.py
@@ -20,16 +20,17 @@ class HashTable:
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
-
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
-        return hash(key)
+        hash = 5381
+        for c in key:
+            hash = (hash * 33) + ord(c)
+        return hash
 
 
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
-
         OPTIONAL STRETCH: Research and implement DJB2
         '''
         hash = 5381
@@ -49,10 +50,7 @@ class HashTable:
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
-        Fill this in.
         '''
         h_key = self._hash_mod(key)
         c_pair = self.storage[h_key]
@@ -72,16 +70,13 @@ class HashTable:
     def remove(self, key):
         '''
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
-        Fill this in.
         '''
         h_key = self._hash_mod(key)
         c_pair = self.storage[h_key]
 
         if c_pair is not None:
-            # basically taking whatever was there and replacing 
+            # basically taking whatever was there and replacing it with none
             self.storage[h_key] = None
         elif c_pair is not None and c_pair.next is not None:
             n_pair = c_pair.next
@@ -93,10 +88,7 @@ class HashTable:
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
-        Fill this in.
         '''
         h_key = self._hash_mod(key)
         c_pair = self.storage[h_key]
@@ -114,20 +106,7 @@ class HashTable:
         '''
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
-
-        Fill this in.
         '''
-        #n_cap = self.capacity * 2
-        #n_table = HashTable(n_cap)
-        #for n_pair in self.storage:
-        #    self.insert(n_pair.key, n_pair.value)
-        #    if n_pair.next:
-        #        c_pair = n_pair.next
-        #        while c_pair:
-        #            self.insert(c_pair.key, c_pair.value)
-        #            c_pair = c_pair.next
-        #return n_table
-
         new_storage = [None] * self.capacity
         self.storage.extend(new_storage)
         
